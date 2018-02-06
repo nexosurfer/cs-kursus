64857d842c84092f09df6b952068e6a29d3c6f15
nexosurfer <mediehorn@gmail.com>
2018-02-06 15:52:53 +0100

    .

---
+++ NedarvingOgPolymporfi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static NedarvingOgPolymporfi.dice;
+
+namespace NedarvingOgPolymporfi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Terning[] terninger = new Terning[5];
+            terninger[0] = new Terning();
+            terninger[1] = new Terning();
+            terninger[2] = new Terning();
+            terninger[3] = new Terning();
+            terninger[4] = new Terning();
+
+            foreach (var item in terninger)
+            {
+                item.Skriv();
+            }
+        }              
+    }
+
+}
---
+++ NedarvingOgPolymporfi/diceapp.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NedarvingOgPolymporfi
+{
+    public class dice
+    {
+
+        public class Terning
+        {
+            private static System.Random rnd;
+            private int værdi;
+
+            public int Værdi
+            {
+                get
+                {
+                    return this.værdi;
+                }
+                set
+                {
+                    if (value < 0 || value > 6)
+                        value = 1;
+                    this.værdi = value;
+
+                }
+            }
+
+            public virtual void Skriv()
+            {
+                Console.WriteLine("[" + this.Værdi + "]");
+            }
+
+            public void Ryst()
+            {
+                this.Værdi = rnd.Next(1, 7);
+            }
+
+            public Terning()
+            {
+                this.Ryst();
+            }
+
+            public Terning(int værdi)
+            {
+                this.Værdi = værdi;
+            }
+
+            static Terning()
+            {
+                rnd = new Random();
+            }
+
+        }
+
+        public class LudoTerning : Terning
+        {
+            public bool ErGlobus()
+            {
+                return this.Værdi == 3;
+            }
+
+            public bool ErStjerne()
+            {
+                return this.Værdi == 5;
+            }
+
+            public LudoTerning() : base()
+            { }
+
+            public LudoTerning (int værdi) : base(værdi)
+            { }
+
+            public override void Skriv()
+            {
+                switch (this.Værdi)
+                {
+                    case 3:
+                        Console.WriteLine("S");
+                        break;
+                    case 5:
+                        Console.WriteLine("G)");
+                        break;
+                    default:
+                        base.Skriv();
+                        break;
+                }
+            }
+        }
+    }
+}
